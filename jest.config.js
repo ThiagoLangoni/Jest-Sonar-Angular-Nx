44bf19354fcdc164c79f32b03437fe4f4056997f
loucoporbike@hotmail.com <loucoporbike@hotmail.com>
2020-11-30 23:54:08 -0300

    fix: coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/mf-app'],
+    moduleFileExtensions: ["js", "ts"],
+    transform: {
+        "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+    },
+    testMatch: ["<rootDir>/tests/**/*.test.ts"],
+    projects: ['<rootDir>/apps/mf-app', '<rootDir>/libs/my-lib', '<rootDir>/libs/my-lib-first'],
 };
